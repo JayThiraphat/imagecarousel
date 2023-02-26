e5866f8fa573c66cec8ab8fe4e3811c339cf60e9
JayThiraphat <wongwatnicha@gmail.com>
2023-02-26 07:24:50 +0700

    commit

---
+++ src/components/ImageData.jsx
@@ -0,0 +1,19 @@
+const ImageData =[
+    {
+        title:"บ้าน",
+        image:"https://cdn.pixabay.com/photo/2023/02/11/13/43/building-7782841_1280.jpg"
+    },
+    {
+        title:"ท้องฟ้า",
+        image:"https://cdn.pixabay.com/photo/2023/01/21/13/39/night-sky-7733876_1280.jpg"
+    },
+    {
+        title:"ทะเล",
+        image:"https://cdn.pixabay.com/photo/2022/03/23/18/56/beach-7087722_1280.jpg"
+    },
+    {
+        title:"ใบไม้",
+        image:"https://cdn.pixabay.com/photo/2023/02/05/17/25/leaves-7770035_1280.jpg"
+    }
+]
+export default ImageData
